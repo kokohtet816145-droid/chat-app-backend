952f0dd6eb86d7540b874e01e42be70f446a394d
kokohtet816145-droid <kokohtet816145@gmail.com>
2026-04-17 19:12:18 +0630

    Add block/unblock logic to backend

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
 let onlineUsers = [];
@@ -22,36 +19,34 @@ io.on('connection', (socket) => {
 
   socket.on('setup', (userId) => {
     socket.join(userId);
-    if (!onlineUsers.some(user => user.userId === userId)) {
+    if (!onlineUsers.some(u => u.userId === userId)) {
       onlineUsers.push({ userId, socketId: socket.id });
     }
     io.emit('get online users', onlineUsers);
   });
 
-  socket.on('join chat', (roomId) => {
-    socket.join(roomId);
+  socket.on('join chat', (roomId) => socket.join(roomId));
+
+  socket.on('new message', (msg) => {
+    socket.to(msg.chat._id).emit('message received', msg);
   });
 
-  socket.on('new message', (newMessage) => {
-    const chat = newMessage.chat;
-    if (!chat.users) return;
-    chat.users.forEach(user => {
-      if (user._id === newMessage.sender._id) return;
-      socket.to(user._id).emit('message received', newMessage);
-    });
-  });
-
-  // Typing Indicator
   socket.on('typing', (room) => socket.to(room).emit('typing', room));
   socket.on('stop typing', (room) => socket.to(room).emit('stop typing', room));
 
+  // Admin: Block/Unblock User
+  socket.on('block user', ({ userId, blockedUserId }) => {
+    socket.to(blockedUserId).emit('user blocked', { by: userId });
+  });
+  socket.on('unblock user', ({ userId, blockedUserId }) => {
+    socket.to(blockedUserId).emit('user unblocked', { by: userId });
+  });
+
   socket.on('disconnect', () => {
-    onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+    onlineUsers = onlineUsers.filter(u => u.socketId !== socket.id);
     io.emit('get online users', onlineUsers);
   });
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
